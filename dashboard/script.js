9b66216a49bae410497fec13acd75782bdbfd168
ThanhTung <thanhtung16601@gmail.com>
2026-01-21 10:50:59 +0700

    update

--- dashboard/script.js
+++ dashboard/script.js
@@ -13,7 +13,6 @@ let DATA_STORE = [];
 function logout() {
   localStorage.removeItem("token");
   localStorage.removeItem("token_exp");
-  window.location.href = "/login.html";
 }
 /**
  * ==============================
